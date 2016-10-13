8f710ccda18587c22596e10ec83c396253b69f15
Ryan Ackermann <ryha26@gmail.com>
2016-10-13 07:29:09 -0500

    added base template rendering

--- app/views.py
+++ app/views.py
@@ -1,6 +1,8 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
-@app.route('/index')
+@app.route('/home')
 def index():
-	return 'Hello World!'
+	user = {'name': 'Ryan'}
+	return render_template('index.html', title='Home', user=user)
