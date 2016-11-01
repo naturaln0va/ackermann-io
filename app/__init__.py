868906fce66ad7c1a53b42ae0cbc83a94c606674
Ryan Ackermann <ryha26@gmail.com>
2016-10-31 19:47:56 -0500

    added heroku config

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,5 @@
 
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_pagedown import PageDown
@@ -21,28 +22,11 @@ from app import views, models
 def prettytime(date):
     return date.strftime("%b %d, %Y")
 
-from config import basedir, ADMINS, MAIL_SERVER, MAIL_PORT, MAIL_USERNAME, MAIL_PASSWORD
-
-# mail support
-
-if not app.debug:
-    import logging
-    from logging.handlers import SMTPHandler
-    credentials = None
-    if MAIL_USERNAME or MAIL_PASSWORD:
-        credentials = (MAIL_USERNAME, MAIL_PASSWORD)
-    mail_handler = SMTPHandler((MAIL_SERVER, MAIL_PORT), 'no-reply@' + MAIL_SERVER, ADMINS, 'ackermann.io failure', credentials)
-    mail_handler.setLevel(logging.ERROR)
-    app.logger.addHandler(mail_handler)
-
 # logging
 
-if not app.debug:
+if os.environ.get('HEROKU') is not None:
     import logging
-    from logging.handlers import RotatingFileHandler
-    file_handler = RotatingFileHandler('tmp/ackermann-io.log', 'a', 1 * 1024 * 1024, 10)
-    file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+    stream_handler = logging.StreamHandler()
+    app.logger.addHandler(stream_handler)
     app.logger.setLevel(logging.INFO)
-    file_handler.setLevel(logging.INFO)
-    app.logger.addHandler(file_handler)
-    app.logger.info('ackermann-io startup')
+    app.logger.info('ackermann.io')
--- config.py
+++ config.py
@@ -9,15 +9,11 @@ SECRET_KEY = 'k?yhGAcb9bFvtH99NYAz[fR87EQFN^6*%q6v'
 
 # database settings
 basedir = os.path.abspath(os.path.dirname(__file__))
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+
+if os.environ.get('DATABASE_URL') is None:
+	SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+else:
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 SQLALCHEMY_TRACK_MODIFICATIONS = True
-
-# mail server settings
-MAIL_SERVER = 'ackermann.io'
-MAIL_PORT = 25
-MAIL_USERNAME = 'ryan@ackermann.io'
-MAIL_PASSWORD = 'donteatZ0mbies'
-
-# administrator list
-ADMINS = ['ryan@ackermann.io']
