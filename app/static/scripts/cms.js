faadd350f1f6c922b7875bfafc250418ef21ce29
Ryan Ackermann <naturaln0va@MightyBook.local>
2016-11-07 07:34:59 -0600

    added copy support to the cms

--- app/static/scripts/cms.js
+++ app/static/scripts/cms.js
@@ -1,7 +1,12 @@
 
 function buttonClicked(elem) {
-	var result = confirm("Are you sure you want to delete '" + elem.name + "'?");
-	if (result == true) {
-		window.location.href = '/cms/rm/' + elem.name;
+	if (elem.value == 'Delete') {
+		var result = confirm("Are you sure you want to delete '" + elem.name + "'?");
+		if (result == true) {
+			window.location.href = '/cms/rm/' + elem.name;
+		}
+	}
+	else if (elem.value == 'Copy') {
+		window.prompt("Copy to clipboard: ⌘+C, Return", "/static/assets/" + elem.name);
 	}
 }
